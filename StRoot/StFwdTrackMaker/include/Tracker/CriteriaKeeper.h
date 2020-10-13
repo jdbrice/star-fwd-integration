59c13a71a4369ecc8f12fcbd02d117467a295a6b
jdb12@rice.edu <jdb12@rice.edu>
2020-10-13 16:21:03 -0400

    add missing KiTrack namespace

--- StRoot/StFwdTrackMaker/include/Tracker/CriteriaKeeper.h
+++ StRoot/StFwdTrackMaker/include/Tracker/CriteriaKeeper.h
@@ -1,11 +1,12 @@
 #ifndef CRITERIA_KEEPER_H
 #define CRITERIA_KEEPER_H
 
+#include "Criteria/Criteria.h"
 
-class CriteriaKeeper : public ICriterion {
+class CriteriaKeeper : public KiTrack::ICriterion {
 
   public:
-    CriteriaKeeper(ICriterion *child) {
+    CriteriaKeeper(KiTrack::ICriterion *child) {
         mChild = child;
         values.clear();
 
@@ -19,7 +20,7 @@ class CriteriaKeeper : public ICriterion {
         mChild = nullptr;
     }
 
-    virtual bool areCompatible(Segment *parent, Segment *child) {
+    virtual bool areCompatible(KiTrack::Segment *parent, KiTrack::Segment *child) {
         bool result = mChild->areCompatible(parent, child);
 
         // capture the primary result of the criteria
@@ -29,8 +30,8 @@ class CriteriaKeeper : public ICriterion {
 
         // two hit criteria
         if ((parent->getHits().size() == 1) && (child->getHits().size() == 1)) {
-            IHit *a = parent->getHits()[0];
-            IHit *b = child->getHits()[0];
+            KiTrack::IHit *a = parent->getHits()[0];
+            KiTrack::IHit *b = child->getHits()[0];
 
             same_track = (static_cast<FwdHit *>(a)->_tid == static_cast<FwdHit *>(b)->_tid && static_cast<FwdHit *>(a)->_tid != 0);
             if (same_track)
@@ -39,9 +40,9 @@ class CriteriaKeeper : public ICriterion {
 
         // three hit criteria (two two-segments)
         if ((parent->getHits().size() == 2) && (child->getHits().size() == 2)) {
-            IHit *a = child->getHits()[0];
-            IHit *b = child->getHits()[1];
-            IHit *c = parent->getHits()[1];
+            KiTrack::IHit *a = child->getHits()[0];
+            KiTrack::IHit *b = child->getHits()[1];
+            KiTrack::IHit *c = parent->getHits()[1];
 
             same_track = (static_cast<FwdHit *>(a)->_tid == static_cast<FwdHit *>(b)->_tid && static_cast<FwdHit *>(b)->_tid == static_cast<FwdHit *>(c)->_tid && static_cast<FwdHit *>(a)->_tid != 0);
             if (same_track)
